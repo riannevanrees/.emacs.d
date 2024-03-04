ff4830739af6bb1d03fb9d06ea380e141b09032b
Rianne van Rees <rianne.vanrees@student.ru.nl>
2024-03-04 15:26:37 +0100

    Included visual fill column mode with a fill-column of 80.

--- init.el
+++ init.el
@@ -13,16 +13,24 @@
 (when (file-exists-p custom-file)
   (load custom-file))
 
+(use-package which-key
+:ensure t
+:config
+(which-key-mode)
+)
+
+(use-package visual-fill-column
+   :ensure t
+   :init
+   (add-hook 'visual-line-mode-hook #'visual-fill-column-mode)
+   :bind ("C-c v" . visual-line-mode)
+   )
+(setq-default fill-column 80)
+
 (use-package modus-themes
 :ensure t
 :config
 (setq modus-themes-italic-constructs t
       modus-themes-bold-constructs t)
 (load-theme 'modus-vivendi-tinted :no-confirm)
-)
-
-(use-package which-key
-:ensure t
-:config
-(which-key-mode)
 )
