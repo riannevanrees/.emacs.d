dc0956855c5ee2d860bee4e7ed93b2ebd60c7f8e
Rianne van Rees <rianne.vanrees@student.ru.nl>
2024-03-13 23:57:54 +0100

    Org customisations and new theme, among others.

--- custom.el
+++ custom.el
@@ -6,7 +6,7 @@
  '(org-agenda-files
    '("~/Documents/tijd/zettelkasten.org" "/Users/rianne/Documents/tijd/studie.org" "/Users/rianne/Documents/tijd/orgel.org" "/Users/rianne/Documents/tijd/schrijven.org" "/Users/rianne/Documents/tijd/vgsn.org" "/Users/rianne/Documents/tijd/tijd.org"))
  '(package-selected-packages
-   '(auctex matlab-mode @ pdf-tools modus-themes markdown-mode better-defaults)))
+   '(org-superstar all-the-icons auctex matlab-mode @ pdf-tools modus-themes markdown-mode better-defaults)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- init.el
+++ init.el
@@ -29,6 +29,13 @@
 
 (setq use-short-answers t) ;; When emacs asks for "yes" or "no", let "y" or "n" suffice
 
+(setq-default line-spacing 1)
+
+(use-package all-the-icons)
+
+(setq backup-directory-alist
+          `((".*" . ,temporary-file-directory)))
+
 (use-package modus-themes
 :ensure t
 :config
@@ -41,3 +48,24 @@
       '((sequence "TODO" "|" "DONE")
 	(sequence "LEZEN" "VERZETTELEN" "|" "VOLTOOID")
 	(sequence "LES" "VERZETTELEN" "|" "VOLTOOID")))
+
+(use-package org-superstar
+  :config
+  (setq org-superstar-leading-bullet " ")
+  (setq org-superstar-special-todo-items t) ;; Makes TODO header bullets into boxes
+  (setq org-superstar-todo-bullet-alist '(("TODO" . 9744)
+					  ("LES" . 9744)
+					  ("LEZEN" . 9744)
+					  ("VERZETTELEN" . 9744)
+					  ("DONE" . 9745)
+					  ("VOLTOOID" . 9745)))
+  )
+
+(use-package org-appear
+  :commands (org-appear-mode)
+  :hook (org-mode . org-appear-mode)
+  :init
+  (setq org-hide-emphasis-markers t		;; A default setting that needs to be t for org-appear
+      org-appear-autoemphasis t		;; Enable org-appear on emphasis (bold, italics, etc)
+      org-appear-autolinks nil		;; Don't enable on links
+      org-appear-autosubmarkers t))	;; Enable on subscript and superscript
