e5bc5075fb0f379fd56321db71c84fc755418a82
Rianne van Rees <rianne.vanrees@student.ru.nl>
2024-04-14 20:24:41 +0200

    Removed some customize-settings.

--- custom.el
+++ custom.el
@@ -8,11 +8,4 @@
  '(org-format-latex-options
    '(:foreground default :background default :scale 2.0 :html-foreground "Black" :html-background "Transparent" :html-scale 1.0 :matchers
 		 ("begin" "$1" "$" "$$" "\\(" "\\[")))
- '(package-selected-packages
-   '(mixed-pitch rainbow-mode autothemer which-key visual-fill-column pdf-tools org-superstar matlab-mode markdown-mode magit julia-mode better-defaults auctex all-the-icons @)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  )
